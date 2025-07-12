9add7bcd6ffb349783681d19e1e94a99d4241ea4
Alex Davidchuk <sasha.d1990@list.ru>
2025-07-12 15:03:46 +0300

    Change data type for date in db

--- weather-consumer/src/main/resources/script.sql
+++ weather-consumer/src/main/resources/script.sql
@@ -2,7 +2,7 @@ CREATE TYPE weather_state AS ENUM ('SUNNY', 'RAINY', 'FOGGY');
 
 CREATE TABLE weather_report (
     id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
+    date TIMESTAMP NOT NULL,
     temperature_celcium SMALLINT NOT NULL,
     weather_state weather_state NOT NULL,
     city VARCHAR(50) NOT NULL
